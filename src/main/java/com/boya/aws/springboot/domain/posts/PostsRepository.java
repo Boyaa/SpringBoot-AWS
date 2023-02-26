95c4c20ee07421935feb60b4f53b6cf47d152fbc
Boyaa <cbh0548@icloud.com>
2023-02-26 20:52:50 +0900

    Add Save, Edit, Delete Function

--- src/main/java/com/boya/aws/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/boya/aws/springboot/domain/posts/PostsRepository.java
@@ -2,8 +2,14 @@ package com.boya.aws.springboot.domain.posts;
 
 import com.boya.aws.springboot.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 // JpaRepository<Entity 클래스, PK타입> 상속하면 기본 CRUD 메소드 자동 생성
 // Entity 클래스와 EntityRepository는 같은 패키지에 위치해야 함
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") // JPA에서 제공하지 않는 메소드를 쿼리로 작성할 수 있게 해줌
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/boya/aws/springboot/service/posts/PostsService.java
+++ src/main/java/com/boya/aws/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.boya.aws.springboot.service.posts;
 
 import com.boya.aws.springboot.domain.posts.Posts;
 import com.boya.aws.springboot.domain.posts.PostsRepository;
+import com.boya.aws.springboot.web.dto.PostsListResponseDto;
 import com.boya.aws.springboot.web.dto.PostsResponseDto;
 import com.boya.aws.springboot.web.dto.PostsSaveRequestDto;
 import com.boya.aws.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +32,28 @@ public class PostsService {
 
         return id;
     }
-
+    @Transactional(readOnly = true)
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다요 id = " + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회 속도 개선됨 (등록, 수정, 삭제 기능이 없을 때 사용)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+        postsRepository.delete(posts); // JPA에서 delete 메소드를 지원한다
+        //deleteById 메소드를 이용하면 id로 삭제할 수도 있다
+        // 존재하는 Posts인지 확인을 위해 엔티티 조회 후 그대로 삭제한다
+    }
+
+
 }
